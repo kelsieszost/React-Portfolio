7529d1ec8cea47c6574a57d2e5b24d5e7df71421
kelsieszost <owner@kelsieszost.design>
2022-12-05 16:32:34 -0500

    interactive carousel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import AboutMe from "./components/About";
+import Gallery from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutMe />
+      <Gallery />
     </div>
   );
 }
