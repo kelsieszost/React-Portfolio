c339abcedac77c5cf2465c1a28843f940d9ab053
kelsieszost <owner@kelsieszost.design>
2022-12-05 12:16:33 -0500

    bootstrap

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Navigation from "./components/Nav";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-// Pass users array to the List component as a prop
 export default function App() {
   return (
     <div>
